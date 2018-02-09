5098bb7514922407274d6b2eb3a6f5e9fc203f65
Luca Pireddu <pireddu@crs4.it>
2018-02-09 12:38:33 +0100

    Parameterize Docker repo and whether to push images in build

--- build_galaxy_stable_init_phenomenal.sh
+++ build_galaxy_stable_init_phenomenal.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -x -e
+
+set -x
+set -o errexit
+set -o nounset
+
+DockerRepo=${DockerRepo:-pcm32}
+DoPush=${DoPush:-false}
 
 ANSIBLE_REPO=galaxyproject/ansible-galaxy-extras
 ANSIBLE_RELEASE=86a127ae3aaaea125c8faa0271471106f2a4f889
@@ -7,14 +13,17 @@ ANSIBLE_RELEASE=86a127ae3aaaea125c8faa0271471106f2a4f889
 GALAXY_RELEASE=b7dc87e872c14878a929237b3cb6d8fe2c579e5d
 GALAXY_REPO=phnmnl/galaxy
 
-docker build --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t pcm32/galaxy-base-pheno ../docker-galaxy-stable/compose/galaxy-base/
-docker push pcm32/galaxy-base-pheno
+docker build --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t ${DockerRepo}/galaxy-base-pheno ../docker-galaxy-stable/compose/galaxy-base/
 
-docker build --build-arg GALAXY_REPO=$GALAXY_REPO --build-arg GALAXY_RELEASE=$GALAXY_RELEASE -t pcm32/galaxy-init-pheno ../docker-galaxy-stable/compose/galaxy-init/
-docker push pcm32/galaxy-init-pheno
+docker build --build-arg GALAXY_REPO=$GALAXY_REPO --build-arg GALAXY_RELEASE=$GALAXY_RELEASE -t ${DockerRepo}/galaxy-init-pheno ../docker-galaxy-stable/compose/galaxy-init/
 
-docker build -t pcm32/galaxy-init-pheno-flavoured -f Dockerfile_init .
-docker push pcm32/galaxy-init-pheno-flavoured
+docker build -t ${DockerRepo}/galaxy-init-pheno-flavoured -f Dockerfile_init .
 
-docker build -t pcm32/galaxy-stable-k8s ../docker-galaxy-stable/compose/galaxy-k8s/
-docker push pcm32/galaxy-stable-k8s
+docker build -t ${DockerRepo}/galaxy-stable-k8s ../docker-galaxy-stable/compose/galaxy-k8s/
+
+if [[ ${DoPush} = true ]]; then
+  docker push ${DockerRepo}/galaxy-base-pheno
+  docker push ${DockerRepo}/galaxy-init-pheno
+  docker push ${DockerRepo}/galaxy-init-pheno-flavoured
+  docker push ${DockerRepo}/galaxy-stable-k8s
+fi
--- simplified_galaxy_stable_container_creation.sh
+++ simplified_galaxy_stable_container_creation.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-#set -x -e
 
-ANSIBLE_REPO=pcm32/ansible-galaxy-extras
+set -x
+set -o errexit
+set -o nounset
+
+DockerRepo=${DockerRepo:-pcm32}
+DoPush=${DoPush:-false}
+
+ANSIBLE_REPO=${DockerRepo}/ansible-galaxy-extras
 ANSIBLE_RELEASE=ee6203507a5bdcf8301d1d09845f204fd4b4e185
 
 TAG=isa_test
@@ -9,8 +15,10 @@ TAG=isa_test
 if [ -n $ANSIBLE_REPO ]
     then
        echo "Making custom galaxy-base-pheno:$TAG from $ANSIBLE_REPO at $ANSIBLE_RELEASE"
-       docker build --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t pcm32/galaxy-base-pheno:$TAG ../docker-galaxy-stable/compose/galaxy-base/
-       docker push pcm32/galaxy-base-pheno:$TAG
+       docker build --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t ${DockerRepo}/galaxy-base-pheno:$TAG ../docker-galaxy-stable/compose/galaxy-base/
+       if [ ${DoPush} = true ]; then
+         docker push ${DockerRepo}/galaxy-base-pheno:$TAG
+       fi
 fi
 
 GALAXY_RELEASE=9a445ed999c1b168690e953d62038e79316f59d2
@@ -22,36 +30,42 @@ if [ -n $GALAXY_REPO ]
        DOCKERFILE_INIT_1=Dockerfile
        if [ -n $ANSIBLE_REPO ]
           then
-              sed s+quay.io/bgruening/galaxy-base:dev+pcm32/galaxy-base-pheno:$TAG+ ../docker-galaxy-stable/compose/galaxy-init/Dockerfile > ../docker-galaxy-stable/compose/galaxy-init/Dockerfile_init
-	      FROM=`grep ^FROM ../docker-galaxy-stable/compose/galaxy-init/Dockerfile_init | awk '{ print $2 }'`
-	      echo "Using FROM $FROM for galaxy init"
-	      DOCKERFILE_INIT_1=Dockerfile_init
+              sed s+quay.io/bgruening/galaxy-base:dev+${DockerRepo}/galaxy-base-pheno:$TAG+ ../docker-galaxy-stable/compose/galaxy-init/Dockerfile > ../docker-galaxy-stable/compose/galaxy-init/Dockerfile_init
+        FROM=`grep ^FROM ../docker-galaxy-stable/compose/galaxy-init/Dockerfile_init | awk '{ print $2 }'`
+        echo "Using FROM $FROM for galaxy init"
+        DOCKERFILE_INIT_1=Dockerfile_init
        fi
-       docker build --build-arg GALAXY_REPO=$GALAXY_REPO --build-arg GALAXY_RELEASE=$GALAXY_RELEASE --build-arg ISATOOLS_LITE_INSTALL=True -t pcm32/galaxy-init-pheno:$TAG -f ../docker-galaxy-stable/compose/galaxy-init/$DOCKERFILE_INIT_1 ../docker-galaxy-stable/compose/galaxy-init/
-       docker push pcm32/galaxy-init-pheno:$TAG
+       docker build --build-arg GALAXY_REPO=$GALAXY_REPO --build-arg GALAXY_RELEASE=$GALAXY_RELEASE --build-arg ISATOOLS_LITE_INSTALL=True -t ${DockerRepo}/galaxy-init-pheno:$TAG -f ../docker-galaxy-stable/compose/galaxy-init/$DOCKERFILE_INIT_1 ../docker-galaxy-stable/compose/galaxy-init/
+       if [ ${DoPush} = true ]; then
+         docker push ${DockerRepo}/galaxy-init-pheno:$TAG
+       fi
 fi
 
 DOCKERFILE_INIT_FLAVOUR=Dockerfile_init
 if [ -n $GALAXY_REPO ]
 then
-	echo "Making custom galaxy-init-pheno-flavoured:$TAG starting from galaxy-init-pheno:$TAG"
-	sed s+pcm32/galaxy-init-pheno$+pcm32/galaxy-init-pheno:$TAG+ Dockerfile_init > Dockerfile_init_tagged
-	DOCKERFILE_INIT_FLAVOUR=Dockerfile_init_tagged
+  echo "Making custom galaxy-init-pheno-flavoured:$TAG starting from galaxy-init-pheno:$TAG"
+  sed s+${DockerRepo}/galaxy-init-pheno$+${DockerRepo}/galaxy-init-pheno:$TAG+ Dockerfile_init > Dockerfile_init_tagged
+  DOCKERFILE_INIT_FLAVOUR=Dockerfile_init_tagged
 fi
-docker build -t pcm32/galaxy-init-pheno-flavoured:$TAG -f $DOCKERFILE_INIT_FLAVOUR .
-docker push pcm32/galaxy-init-pheno-flavoured:$TAG
+docker build -t ${DockerRepo}/galaxy-init-pheno-flavoured:$TAG -f $DOCKERFILE_INIT_FLAVOUR .
+if [ ${DoPush} = true ]; then
+  docker push ${DockerRepo}/galaxy-init-pheno-flavoured:$TAG
+fi
 
 
 DOCKERFILE_WEB=Dockerfile
 if [ -n $GALAXY_REPO ]
 then
-	echo "Making custom galaxy-web-k8s:$TAG from $GALAXY_REPO at $GALAXY_RELEASE"
-	sed s+quay.io/bgruening/galaxy-base:dev+pcm32/galaxy-base-pheno:$TAG+ ../docker-galaxy-stable/compose/galaxy-web/Dockerfile > ../docker-galaxy-stable/compose/galaxy-web/Dockerfile_web
-	DOCKERFILE_WEB=Dockerfile_web
+  echo "Making custom galaxy-web-k8s:$TAG from $GALAXY_REPO at $GALAXY_RELEASE"
+  sed s+quay.io/bgruening/galaxy-base:dev+${DockerRepo}/galaxy-base-pheno:$TAG+ ../docker-galaxy-stable/compose/galaxy-web/Dockerfile > ../docker-galaxy-stable/compose/galaxy-web/Dockerfile_web
+  DOCKERFILE_WEB=Dockerfile_web
 fi
-docker build --build-arg GALAXY_ANSIBLE_TAGS=supervisor,startup,scripts,nginx,k8,k8s -t pcm32/galaxy-web-k8s:$TAG -f ../docker-galaxy-stable/compose/galaxy-web/$DOCKERFILE_WEB ../docker-galaxy-stable/compose/galaxy-web/
-docker push pcm32/galaxy-web-k8s:$TAG
+docker build --build-arg GALAXY_ANSIBLE_TAGS=supervisor,startup,scripts,nginx,k8,k8s -t ${DockerRepo}/galaxy-web-k8s:$TAG -f ../docker-galaxy-stable/compose/galaxy-web/$DOCKERFILE_WEB ../docker-galaxy-stable/compose/galaxy-web/
+if [ ${DoPush} = true ]; then
+  docker push ${DockerRepo}/galaxy-web-k8s:$TAG
+fi
 
 echo "Relevant containers:"
-echo "Web:          pcm32/galaxy-web-k8s:$TAG"
-echo "Init Flavour: pcm32/galaxy-init-pheno-flavoured:$TAG"
+echo "Web:          ${DockerRepo}/galaxy-web-k8s:$TAG"
+echo "Init Flavour: ${DockerRepo}/galaxy-init-pheno-flavoured:$TAG"
